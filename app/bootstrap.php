913d587f412c8f1a28839a87118b39858b727c03
Thijs Cramer <thijs.cramer@gmail.com>
2020-02-23 15:15:40 +0100

    Fix composer install; Remove Edit on Row click

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,6 +10,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\HttpFragmentServiceProvider());
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new Silex\Provider\TranslationServiceProvider(), array(
         'translator.messages' => array(),
